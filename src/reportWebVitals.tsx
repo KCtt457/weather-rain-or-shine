5c33127fdd1c8acfd71c9b5a3f37ff34763af660
KCtt457 <chinkam123@gmail.com>
2022-11-27 18:18:38 -0500

    add typescript to project

--- src/reportWebVitals.tsx
+++ src/reportWebVitals.tsx
@@ -1,4 +1,6 @@
-const reportWebVitals = onPerfEntry => {
+type VoidFunction = () => void;
+
+const reportWebVitals = (onPerfEntry: VoidFunction) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
